8a41625cbcc29176f786481432d8509dd1b4db99
Norah <nourelhoudaromdhani@esprit.tn>
2025-10-20 20:48:41 +0100

    CORS is configured for requests from http://localhost:4200

--- src/main/java/tn/romdhani/managementsystem/security/CorsConfig.java
+++ src/main/java/tn/romdhani/managementsystem/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8089")
+                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // 👈 allow Authorization header later
